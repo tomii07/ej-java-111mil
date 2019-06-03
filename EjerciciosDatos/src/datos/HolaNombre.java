935f58f8584997d4915020f47cc7ebbb95333a72
tomii07 <tomy08086@gmail.com>
2019-06-03 18:30:19 -0300

    Ejercicios

---
+++ EjerciciosDatos/src/datos/HolaNombre.java
@@ -0,0 +1,10 @@
+package datos;
+import java.util.Scanner;
+public class HolaNombre {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su nombre: ");
+        String nombre = sc.next();
+        System.out.println("Hola, " + nombre);
+    }
+}
